1c5624e3488314047c10cdaaa3a68f1aa352a234
Toffee35 <nailzagru@gmail.com>
2026-07-04 21:44:37 +0300

    feat: отображение сессии пользователя на главной странице фронтенда

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,8 +1,26 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { getRequestHeader } from "@tanstack/react-start/server";
+
+const getUserSession = createServerFn({ method: "GET" }).handler(() => {
+  return getRequestHeader("X-User-Session") ?? "null";
+});
 
 export const Route = createFileRoute("/")({
   head: () => ({
     meta: [{ title: "Hello World!" }],
   }),
-  component: () => <h1>Hello World!</h1>,
+  loader: () => getUserSession(),
+  component: HomePage,
 });
+
+function HomePage() {
+  const userSession = Route.useLoaderData() as string;
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <pre>{JSON.stringify(JSON.parse(userSession), null, 4)}</pre>
+    </div>
+  );
+}
